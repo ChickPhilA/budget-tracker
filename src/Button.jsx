e13deecc8ed82ee70bd79da9c9d8fd14b8b73613
Phil <natsudragoneel282003@gmail.com>
2026-02-21 16:23:45 -0800

    Created a Button component to be used across application

---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+function Button({text}) {
+    return(
+        <>
+            <button className="bg-emerald-500 text-white px-6 py-2 mb-4 rounded-lg font-bold">
+                {text}
+            </button>
+        </>
+    );
+}
+
+export default Button
