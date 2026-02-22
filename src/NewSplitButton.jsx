65da36fb580fdbfed4e71c35282bb8ff80558a63
Phil <natsudragoneel282003@gmail.com>
2026-02-21 16:24:05 -0800

    Created a NewSplitButton to calculate paycheck splits, but has no clickable functionality for now

---
+++ src/NewSplitButton.jsx
@@ -0,0 +1,14 @@
+import Button from './Button.jsx'
+
+function NewSplitButton() {
+    const label = "Start a new pay split"
+
+    return (
+        <>
+            <h1 class="text-2xl text-[rgb(173,216,230)] my-4"> {"Did you recently get paid?"} </h1>
+            <Button text= {label} />
+        </>
+    );
+}
+
+export default NewSplitButton
