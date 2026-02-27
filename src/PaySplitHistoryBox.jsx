46cc76d6b65d611ce30957fd2e7084fda1f13ec1
Phil <natsudragoneel282003@gmail.com>
2026-02-27 15:59:46 -0800

    Removed split history box and aded it to App.jsx file to make delivery of data efficient

--- src/PaySplitHistoryBox.jsx
+++ src/PaySplitHistoryBox.jsx
@@ -1,16 +1,4 @@
-import { useState } from 'react'
-
-const fakePaySplits = [ 
-  {id: 1, Date: '(02/06/2026)', pay: 660.36},
-  {id: 2, Date: '(02/13/2026)', pay: 734.92},
-  {id: 3, Date: '(02/20/2026)', pay: 553.18}
-] // these are to display within the history cotainer below. REMINDER: JS DOES MONTHS 0 TO 11
-
-
-
-export function PaySplitHistoryBox() {
-
-  const [history, changeHistory] = useState(fakePaySplits)
+export function PaySplitHistoryBox({history}) {
 
     return (
         <>
@@ -21,7 +9,8 @@ export function PaySplitHistoryBox() {
                     <p className="text-2xl font-bold mx-auto"> You have no pay splits available! </p> : 
                     history.map((payStub) =>
                         // here, we create a new box below
-                        <div className="flex flex-row justify-between bg-gray-700 max-w-3xl px-4 py-4 rounded-lg mb-4 border border-gray-600" key={payStub.id}>
+                        <div className="transition-all hover:scale-105 duration-300 ease-in-out hover:bg-emerald-700
+                        flex flex-row justify-between bg-gray-700 max-w-3xl px-4 py-4 rounded-lg mx-auto mb-4 border border-gray-600" key={payStub.id}>
                             <p className="text-md font-semibold"> Payday of {payStub.Date} </p>
                             <p className="text-md font-semibold"> Pay Amount: ${payStub.pay} </p>
                         </div>
