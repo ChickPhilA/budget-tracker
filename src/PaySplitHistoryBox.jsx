6d12a4ecb7835758d8dda875e48eba67a1c91cc3
Phil <natsudragoneel282003@gmail.com>
2026-03-08 19:00:33 -0700

    No functionality has been added yet, but I added a trash can SVG for future delete functionality

--- src/PaySplitHistoryBox.jsx
+++ src/PaySplitHistoryBox.jsx
@@ -1,3 +1,5 @@
+import { Trash2 } from "lucide-react";
+
 export function PaySplitHistoryBox({history}) {
 
     return (
@@ -11,8 +13,19 @@ export function PaySplitHistoryBox({history}) {
                         // here, we create a new box below
                         <div className="transition-all hover:scale-105 duration-300 ease-in-out hover:bg-emerald-700
                         flex flex-row justify-between bg-gray-700 max-w-3xl px-4 py-4 rounded-lg mx-auto mb-4 border border-gray-600" key={payStub.id}>
-                            <p className="text-md font-semibold"> Payday of {payStub.Date} </p>
-                            <p className="text-md font-semibold"> Pay Amount: ${payStub.pay} </p>
+                            <div>
+                                <p className="text-md font-semibold"> Payday of {payStub.Date} </p>
+                            </div>
+
+                            <div className="flex flex-row items-center gap-4 text-gray-300">
+                                <p className="text-md font-semibold"> Pay Amount: ${payStub.pay} </p>
+
+                                <div className="h-6 w-px bg-gray-500"></div>
+
+                                <button className="text-gray-400" title="Delete this pay split">
+                                    <Trash2 className="cursor-pointer hover:text-red-500" size={20} />
+                                </button>
+                            </div>
                         </div>
                     )
                 }
