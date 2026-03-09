48fa5aef5aa7e750731166d35206a7be9605b906
Phil <natsudragoneel282003@gmail.com>
2026-03-08 19:00:03 -0700

    Added transition hover effect to Add New Pay Split button

--- src/NewSplitButton.jsx
+++ src/NewSplitButton.jsx
@@ -1,12 +1,12 @@
 import Button from './Button.jsx'
 
-function NewSplitButton() {
+function NewSplitButton({paySplitScreenOpen}) {
     const label = "Start a new pay split"
 
     return (
         <>
-            <h1 className="text-2xl text-[rgb(173,216,230)] my-4"> {"Did you recently get paid?"} </h1>
-            <Button text= {label} />
+            <h1 className="text-2xl text-[rgb(100,110,113)] my-4"> {"Did you recently get paid?"} </h1>
+            <Button text= {label} onclick={paySplitScreenOpen} />
         </>
     );
 }
