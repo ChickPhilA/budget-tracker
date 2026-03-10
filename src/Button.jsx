6b61b1a495cd36a426828d40fe7c567e1203e1cd
Phil <natsudragoneel282003@gmail.com>
2026-03-09 18:15:13 -0700

    Added onClick property to wire up the button's native event listener being passed

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,10 @@
-function Button({text}) {
+function Button({text, onClick}) {
     return(
         <>
-            <button className="bg-emerald-500 text-white px-6 py-2 mb-4 rounded-lg font-bold">
+            <button className="bg-emerald-500 hover:bg-emerald-700  hover:cursor-pointer active:scale-95 transition-all 
+            duration-300 ease-in-out text-white px-6 py-2 mb-4 rounded-lg font-bold"
+            onClick={onClick}
+            >
                 {text}
             </button>
         </>
