1f98d409696b81aa3df5b7f1fe0e3b04ddfe789e
Phil <natsudragoneel282003@gmail.com>
2026-03-09 18:15:39 -0700

    Fixed camelcase for onClick to compile jsx syntax

--- src/NewSplitButton.jsx
+++ src/NewSplitButton.jsx
@@ -6,7 +6,7 @@ function NewSplitButton({paySplitScreenOpen}) {
     return (
         <>
             <h1 className="text-2xl text-[rgb(100,110,113)] my-4"> {"Did you recently get paid?"} </h1>
-            <Button text= {label} onclick={paySplitScreenOpen} />
+            <Button text= {label} onClick={paySplitScreenOpen} />
         </>
     );
 }
